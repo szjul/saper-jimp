31669ba839e4ccb989c49d9e3a66b1e9a6e8c2b2
Matylda Drapich <01197793@pw.edu.pl>
2025-01-27 17:55:24 +0000

    dodano funkcje do obslugi pliku oraz poprawione funkcje do pobierania nazw graczy

--- obsluga.c
+++ obsluga.c
@@ -2,31 +2,76 @@
 #include <stdlib.h>
 #include <string.h>
 #include "obsluga.h"
+#include "saper.h"
 
+#define MAX_HIGHSCORE_ENTRIES 10
 
-void wybor_poziomu()
+Level wybor_poziomu(char* bufor)
 {
-
-}
-
-
-void dodaj_gracza(Gracz gracze[], int *liczba_graczy, const char *imie, int czas) {
-	if (*liczba_graczy < 5)
+	if(strcmp(bufor,"latwy") == 0 )
 	{
-	strncpy(gracze[*liczba_graczy].imie, imie, sizeof(gracze[*liczba_graczy].imie) - 1);
-	gracze[*liczba_graczy].czas = czas;
-	(*liczba_graczy)++;
-	}	
-	else 
-	{
-		printf("Brak miejsca na nowych graczy!\n");
+		return EASY_LEVEL;
+        } else if(strcmp(bufor,"sredni") == 0)
+        {
+        	return MEDIUM_LEVEL;
+       	} else if(strcmp(bufor, "trudny") == 0)
+        {
+		return HARD_LEVEL;
+        } else if(strcmp(bufor, "custom") == 0)
+        {
+		return CUSTOM_LEVEL;
 	}
+	return UNKNOWN_LEVEL;
 }
 
-void wyswietl_graczy(Gracz gracze[], int liczba_graczy) {
-	printf("\nNajlepsi gracze:\n");
+void zapisz_wyniki_do_pliku(const char* nazwa_pliku, Gracz* gracze, int liczba_graczy) 
+{
+	FILE *plik = fopen(nazwa_pliku, "a");
+	if (plik == NULL)
+	{
+		printf("Nie mozna otworzyc pliku do zapisu.\n");
+		return;
+	}
 	for (int i = 0; i < liczba_graczy; i++)
 	{
-		printf("%d. %s - %d sekund\n", i + 1, gracze[i].imie, gracze[i].czas);
+		fprintf(plik, "%s %d\n", gracze[i].imie, gracze[i].wynik);
+	}
+	fclose(plik);
+};
+
+int porownaj_graczy(const void* a, const void* b)
+{
+	return ((Gracz*)b)->wynik - ((Gracz*)a)->wynik;
+}
+
+void wyswietl_najlepszych_graczy(const char* nazwa_pliku)
+{
+	FILE *plik = fopen(nazwa_pliku, "r");
+	if (plik == NULL)
+	{
+		printf("Nie mozna otworzyc pliku do odczytu.\n");
+		return;
+	}
+
+	Gracz gracze[MAX_HIGHSCORE_ENTRIES];
+	int liczba_graczy = 0;
+
+	while (fscanf(plik, "%s %d", gracze[liczba_graczy].imie, &gracze[liczba_graczy].wynik) != EOF)
+	{
+		liczba_graczy++;
+		if (liczba_graczy>=MAX_HIGHSCORE_ENTRIES)
+		{
+			printf("Lista graczy pelna!\n:");
+			break;
+		}
+	}
+
+	fclose(plik);
+
+	qsort(gracze, liczba_graczy, sizeof(Gracz), porownaj_graczy);
+	printf("5 najlepszych graczy:\n");
+	for (int i = 0; i < 5 && i < liczba_graczy; i++)
+	{
+		printf("%d. %s - %d\n", i + 1, gracze[i].imie, gracze[i].wynik);
 	}
 }
--- obsluga.h
+++ obsluga.h
@@ -1,14 +1,20 @@
 #ifndef _OBSLUGA_H_IS_INCLUDED_
 #define _OBSLUGA_H_IS_INCLUDED_
+#include "saper.h"
 
-typedef struct 
-{	
-	char imie[50];
-	int czas;
-} Gracz;
+typedef enum
+{
+        UNKNOWN_LEVEL,
+        EASY_LEVEL,
+        MEDIUM_LEVEL,
+        HARD_LEVEL,
+        CUSTOM_LEVEL
+} Level;
 
-void dodaj_gracza(Gracz gracze[], int *liczba_graczy, const char *imie, int czas);
-void wyswietl_graczy(Gracz gracze[], int liczba_graczy);
+void zapisz_wyniki_do_pliku(const char* nazwa_pliku, Gracz* gracze, int liczba_graczy);
+int porownaj_graczy(const void* a, const void* b);
+void wyswietl_najlepszych_graczy(const char* nazwa_pliku);
+Level wybor_poziomu(char* poziom);
 
 
 #endif
