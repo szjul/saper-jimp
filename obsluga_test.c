16b75ea728ff63bb6871a9b47de1e45c25400260
Matylda Drapich <01197793@pw.edu.pl>
2025-01-28 14:27:58 +0000

    dalsze testy

--- obsluga_test.c
+++ obsluga_test.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 #include "saper.h"
 #include "obsluga.h"
 
@@ -13,3 +14,47 @@ void testuj_wybor_poziomu()
 	assert(wybor_poziomu("inny")==UNKNOWN_LEVEL);
 }
 
+
+void testuj_zapisz_wyniki_do_pliku()
+{
+
+	Gracz gracze[] = { {"Ola", 15}, {"Kasia", 9}, {"Kuba", 2} };
+	int liczba_graczy = 3;
+	Gracz gracze_odcz[liczba_graczy];
+	int nr_gracza = 0;
+	const char* testowy_plik = "testowe_wyniki.txt";
+
+	zapisz_wyniki_do_pliku(testowy_plik, gracze, liczba_graczy);
+
+	FILE * plik =fopen(testowy_plik, "r");
+	assert(plik!=NULL);
+	while (fscanf(plik, "%s %d", gracze_odcz[nr_gracza].imie, &gracze_odcz[nr_gracza].wynik) != EOF)
+        {
+                nr_gracza++;
+               
+        }
+	int i;
+        fclose(plik);
+	assert(liczba_graczy == nr_gracza);
+	for( i =0; i<liczba_graczy;i++);
+	{
+	assert(strcmp(gracze_odcz[i].imie,gracze[i].imie)==0);
+	assert(gracze_odcz[i].wynik ==gracze[i].wynik);
+
+	}
+
+}
+
+void testuj_wyswietl_najlepszych_graczy()
+{
+	Gracz gracze[] = { {"Ola", 6}, {"Kasia", 9}, {"Kuba", 7} };
+        int liczba_graczy = 3;
+        Gracz gracze_odcz[liczba_graczy];
+        int nr_gracza = 0;
+        const char* testowy_plik = "testowe_wyniki2.txt";
+
+        zapisz_wyniki_do_pliku(testowy_plik, gracze, liczba_graczy);
+	wyswietl_najlepszych_graczy(testowy_plik);
+
+
+}
--- sapertest.c
+++ sapertest.c
@@ -5,13 +5,19 @@
 
 void testuj_wybor_poziomu(void);
 void testuj_gra_z_pliku(void);
+void testuj_zapisz_wyniki_do_pliku();
+void testuj_wyswietl_najlepszych_graczy();
 
 int main(int argc, char * argv[])
 {
 	printf("Testuje: wybor_poziomu.\n");
 	testuj_wybor_poziomu();
-	printf("Testuje: gra_z_pliku.\n");
-	testuj_gra_z_pliku();
+	//printf("Testuje: gra_z_pliku.\n");
+	//testuj_gra_z_pliku();
+	printf("testuje: zapisz_wyniki_do_pliku.\n");
+	testuj_zapisz_wyniki_do_pliku();
+	printf("Testuje: wyswietl_najlepszych_graczy.\n");
+	testuj_wyswietl_najlepszych_graczy();
 
 	printf("Testy wykonane poprawnie\n");
 }
